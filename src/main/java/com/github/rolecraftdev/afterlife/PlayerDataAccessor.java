8cc0e0a7298e71b79c62c79a55a8285c20d20d72
mythbusterma <wiirocksme@gmail.com>
2014-07-30 19:44:03 -0400

    Listener, Accessor and gitignore

--- src/main/java/com/github/rolecraftdev/afterlife/PlayerDataAccessor.java
+++ src/main/java/com/github/rolecraftdev/afterlife/PlayerDataAccessor.java
@@ -1,11 +1,21 @@
 package com.github.rolecraftdev.afterlife;
 
+import org.bukkit.entity.Player;
+
 public class PlayerDataAccessor {
 
     private RolecraftAfterlife plugin;
 
     public PlayerDataAccessor(RolecraftAfterlife plugin) {
         this.plugin = plugin;
+    }
+    
+    public void addKarma (final Player ply, final float amount)  {
+        plugin.getCore().getDataManager().getPlayerData(ply.getUniqueId()).addKarma(amount);
+    }
+    
+    public void deductKarma (final Player ply, final float amount) {
+        plugin.getCore().getDataManager().getPlayerData(ply.getUniqueId()).deductKarma(amount);
     }
 
 }
---
+++ src/main/java/com/github/rolecraftdev/afterlife/listener/PickupListener.java
@@ -0,0 +1,17 @@
+package com.github.rolecraftdev.afterlife.listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+
+public class PickupListener implements Listener {
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onPickup() {
+        
+    }
+    
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onDrop() {
+        
+    }
+}
