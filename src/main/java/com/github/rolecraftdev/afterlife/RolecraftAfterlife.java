539a0464a6aee6326acb07ead0580dcec9684580
mythbusterma <wiirocksme@gmail.com>
2014-07-30 17:53:32 -0400

    Plugin.yml

--- src/main/java/com/github/rolecraftdev/afterlife/RolecraftAfterlife.java
+++ src/main/java/com/github/rolecraftdev/afterlife/RolecraftAfterlife.java
@@ -1,5 +1,25 @@
 package com.github.rolecraftdev.afterlife;
 
-public class RolecraftAfterlife {
+import org.bukkit.plugin.java.JavaPlugin;
 
+import com.github.rolecraftdev.RolecraftCore;
+
+public class RolecraftAfterlife extends JavaPlugin {
+    
+    private RolecraftCore corePlugin;
+    
+    @Override
+    public void onLoad() {
+        
+    }
+    
+    @Override
+    public void onEnable () {
+        
+    }
+    
+    @Override
+    public void onDisable () {
+        
+    }
 }
